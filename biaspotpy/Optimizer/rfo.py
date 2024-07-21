ab9f94d81e6ff84a009a99d9d9e1a3e442cb3fb1
ss0832 <80376408+ss0832@users.noreply.github.com>
2024-07-21 11:59:36 +0900

    Add files via upload

--- biaspotpy/Optimizer/rfo.py
+++ biaspotpy/Optimizer/rfo.py
@@ -24,6 +24,47 @@ class RationalFunctionOptimization:
         self.beta = 0.5
         return
     
+    def calc_center(self, geomerty, element_list=[]):#geomerty:Bohr
+        center = np.array([0.0, 0.0, 0.0], dtype="float64")
+        for i in range(len(geomerty)):
+            
+            center += geomerty[i] 
+        center /= float(len(geomerty))
+        
+        return center
+            
+    def project_out_hess_tr_and_rot_for_coord(self, hessian, geomerty):#do not consider atomic mass
+        natoms = len(geomerty)
+       
+        geomerty -= self.calc_center(geomerty)
+        
+    
+        tr_x = (np.tile(np.array([1, 0, 0]), natoms)).reshape(-1, 3)
+        tr_y = (np.tile(np.array([0, 1, 0]), natoms)).reshape(-1, 3)
+        tr_z = (np.tile(np.array([0, 0, 1]), natoms)).reshape(-1, 3)
+
+        rot_x = np.cross(geomerty, tr_x).flatten()
+        rot_y = np.cross(geomerty, tr_y).flatten() 
+        rot_z = np.cross(geomerty, tr_z).flatten()
+        tr_x = tr_x.flatten()
+        tr_y = tr_y.flatten()
+        tr_z = tr_z.flatten()
+
+        TR_vectors = np.vstack([tr_x, tr_y, tr_z, rot_x, rot_y, rot_z])
+        
+        Q, R = np.linalg.qr(TR_vectors.T)
+        keep_indices = ~np.isclose(np.diag(R), 0, atol=1e-6, rtol=0)
+        TR_vectors = Q.T[keep_indices]
+        n_tr = len(TR_vectors)
+
+        P = np.identity(natoms * 3)
+        for vector in TR_vectors:
+            P -= np.outer(vector, vector)
+
+        hess_proj = np.dot(np.dot(P.T, hessian), P)
+
+        return hess_proj    
+    
     def set_hessian(self, hessian):
         self.hessian = hessian
         return
@@ -60,7 +101,7 @@ class RationalFunctionOptimization:
         DELTA_for_QNM = self.DELTA
         
         delta_hess = self.hessian_update(displacement, delta_grad)
-
+        
         
         if self.iter % self.FC_COUNT != 0 or self.FC_COUNT == -1:
             new_hess = self.hessian + delta_hess + self.bias_hessian
@@ -76,7 +117,7 @@ class RationalFunctionOptimization:
         RFO_eigenvalue = np.sort(RFO_eigenvalue)
         lambda_for_calc = float(RFO_eigenvalue[self.saddle_order])
 
-        hess_eigenvalue, hess_eigenvector = np.linalg.eig(new_hess)
+        hess_eigenvalue, hess_eigenvector = np.linalg.eigh(new_hess)
         hess_eigenvector = hess_eigenvector.T
         move_vector = np.zeros((len(geom_num_list)*3, 1))
         DELTA_for_QNM = self.DELTA
@@ -97,9 +138,56 @@ class RationalFunctionOptimization:
      
         return move_vector#Bohr.
         
+    def normal_v3(self, geom_num_list, B_g, pre_B_g, pre_geom, B_e, pre_B_e, pre_g, g):
+        #ref.:Culot, P., Dive, G., Nguyen, V.H. et al. A quasi-Newton algorithm for first-order saddle-point location. Theoret. Chim. Acta 82, 189–205 (1992). https://doi.org/10.1007/BF01113492
+        if self.Initialization:
+            self.Initialization = False
+            return -1*self.DELTA*B_g
+        print("saddle order:", self.saddle_order)
+        delta_grad = (g - pre_g).reshape(len(geom_num_list)*3, 1)
+        displacement = (geom_num_list - pre_geom).reshape(len(geom_num_list)*3, 1)
+        DELTA_for_QNM = self.DELTA
+        
+        delta_hess = self.hessian_update(displacement, delta_grad)
+        delta_hess = self.project_out_hess_tr_and_rot_for_coord(delta_hess, geom_num_list)
+        
+        if self.iter % self.FC_COUNT != 0 or self.FC_COUNT == -1:
+            new_hess = self.hessian + delta_hess + self.bias_hessian
+        else:
+            new_hess = self.hessian + self.bias_hessian
+        
+        
+        matrix_for_RFO = np.append(new_hess, B_g.reshape(len(geom_num_list)*3, 1), axis=1)
+        tmp = np.array([np.append(B_g.reshape(1, len(geom_num_list)*3), 0.0)], dtype="float64")
+        
+        matrix_for_RFO = np.append(matrix_for_RFO, tmp, axis=0)
+        RFO_eigenvalue, _ = np.linalg.eigh(matrix_for_RFO)
+        RFO_eigenvalue = np.sort(RFO_eigenvalue)
+        lambda_for_calc = float(RFO_eigenvalue[max(self.saddle_order-1, 0)])
+
+        hess_eigenvalue, hess_eigenvector = np.linalg.eigh(new_hess)
+        hess_eigenvector = hess_eigenvector.T
+        hess_eigenval_indices = np.argsort(hess_eigenvalue)
+        
+        move_vector = np.zeros((len(geom_num_list)*3, 1))
+        DELTA_for_QNM = self.DELTA
+        
+        for i in range(len(hess_eigenvalue)):
+            tmp_vector = np.array([hess_eigenvector[hess_eigenval_indices[i]].T], dtype="float64")
+            if i < self.saddle_order:
+                move_vector += DELTA_for_QNM * np.dot(tmp_vector, B_g.reshape(len(geom_num_list)*3, 1)) * tmp_vector.T / (hess_eigenvalue[hess_eigenval_indices[i]] + lambda_for_calc + 1e-8) 
+            else:
+                move_vector += DELTA_for_QNM * np.dot(tmp_vector, B_g.reshape(len(geom_num_list)*3, 1)) * tmp_vector.T / (hess_eigenvalue[hess_eigenval_indices[i]] - lambda_for_calc + 1e-8)
+        print("lambda   : ",lambda_for_calc)
+        print("step size: ",DELTA_for_QNM)
+        move_vector = move_vector.reshape(len(geom_num_list), 3)
+        self.hessian += delta_hess 
+        self.iter += 1
+     
+        return move_vector#Bohr.
     
     def normal(self, geom_num_list, B_g, pre_B_g, pre_geom, B_e, pre_B_e, pre_g, g):
-        print("normal mode")
+       
         if self.Initialization:
             self.Initialization = False
             return -1*self.DELTA*B_g
@@ -203,6 +291,8 @@ class RationalFunctionOptimization:
             move_vector = self.moment(geom_num_list, B_g, pre_B_g, pre_geom, B_e, pre_B_e, pre_g, g)
         elif "RFO2" in self.config["method"]:
             move_vector = self.normal_v2(geom_num_list, B_g, pre_B_g, pre_geom, B_e, pre_B_e, pre_g, g)
+        elif "RFO3" in self.config["method"]:
+            move_vector = self.normal_v3(geom_num_list, B_g, pre_B_g, pre_geom, B_e, pre_B_e, pre_g, g)
         else:
             move_vector = self.normal(geom_num_list, B_g, pre_B_g, pre_geom, B_e, pre_B_e, pre_g, g)
         return move_vector
