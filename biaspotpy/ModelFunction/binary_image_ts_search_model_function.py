aee59de982351d9a3d998287fc43771578d27677
ss0832 <80376408+ss0832@users.noreply.github.com>
2024-09-15 18:48:58 +0900

    Add files via upload

--- biaspotpy/ModelFunction/binary_image_ts_search_model_function.py
+++ biaspotpy/ModelFunction/binary_image_ts_search_model_function.py
@@ -5,7 +5,7 @@ import numpy as np
 
 class BITSSModelFunction:
     def __init__(self, geom_num_list_1, geom_num_list_2):
-        self.f = 0.5
+        self.f = 0.4
         self.alpha = 10.0
         self.beta = 0.1
         self.d = np.linalg.norm(geom_num_list_1 - geom_num_list_2)
@@ -17,7 +17,7 @@ class BITSSModelFunction:
         if iter % 100 == 0:
 
             self.E_B = abs(energy_1 - energy_2)
-            self.kappa_e = self.alpha / (2.0 * self.E_B)
+            self.kappa_e = self.alpha / (2.0 * self.E_B + 1e-10)
         
             unit_vec = (geom_num_list_1 - geom_num_list_2) / current_distance
             
@@ -26,18 +26,19 @@ class BITSSModelFunction:
             proj_grad_1 = gradient_1 * unit_vec * (-1)
             proj_grad_2 = gradient_2 * unit_vec
 
-            a = np.sqrt(np.linalg.norm(proj_grad_1) + np.linalg.norm(proj_grad_2)) / (2 ** 1.5 * self.beta * self.d)
-            b = self.E_B / (self.beta * self.d ** 2)
+            a = np.sqrt(np.linalg.norm(proj_grad_1) + np.linalg.norm(proj_grad_2)) / (2 ** 1.5 * self.beta * self.d + 1e-10)
+            b = self.E_B / (self.beta * self.d ** 2 + 1e-10)
             self.kappa_d = max(a, b)
+            self.d = np.linalg.norm(geom_num_list_1 - geom_num_list_2)
         
-        self.d = (1.0 - self.f) * self.d
+        self.d = max((1.0 - self.f) * self.d, 1e-10)
         energy = energy_1 + energy_2 + self.kappa_e * (energy_1 + energy_2) ** 2 + self.kappa_d * (current_distance - self.d) ** 2
         
         return energy
         
     def calc_grad(self, energy_1, energy_2, geom_num_list_1, geom_num_list_2, gradient_1, gradient_2):
         current_vec = geom_num_list_1 - geom_num_list_2
-        current_dist = np.linalg.norm(current_vec)
+        current_dist = np.linalg.norm(current_vec) + 1e-10
 
         bitss_grad_1 = gradient_1 + 2.0 * self.kappa_e * (energy_1 - energy_2) * gradient_1 + current_vec * 2.0 * self.kappa_d * (current_dist - self.d) / current_dist
 
