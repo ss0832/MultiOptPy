1db57cb1a4fd2ff835995b41c305e7613b1ff281
ss0832 <80376408+ss0832@users.noreply.github.com>
2024-10-15 18:06:03 +0900

    Add files via upload

--- optmain.py
+++ optmain.py
@@ -1,9 +1,17 @@
 import sys
+#import os
+#from glob import glob
 sys.path.append('./biaspotpy')
 
 import biaspotpy
 
 parser = biaspotpy.interface.init_parser()
-args = biaspotpy.interface.optimizeparser(parser)
+args = biaspotpy.interface.optimizeparser(parser)    
 bpa = biaspotpy.optimization.Optimize(args)
 bpa.run()
+#print("----------------------------------------------------")
+#print("### gradients of bias potential parameters       ###")
+#print("### (This is the parameters of the final file.)  ###")
+#print(bpa.bias_pot_params_grad_name_list)
+#print(bpa.bias_pot_params_grad_list)
+#print("----------------------------------------------------")
