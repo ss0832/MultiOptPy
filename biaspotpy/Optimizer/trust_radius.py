db7884042ac38748bffb6488223b4d7a85b9586d
ss0832 <80376408+ss0832@users.noreply.github.com>
2024-10-19 12:17:49 +0900

    Update trust_radius.py

--- biaspotpy/Optimizer/trust_radius.py
+++ biaspotpy/Optimizer/trust_radius.py
@@ -4,7 +4,7 @@ import numpy as np
 def update_trust_radii(B_e, pre_B_e, pre_B_g, pre_move_vector, model_hess, geom_num_list, trust_radii, trust_radii_update):
     if trust_radii_update == "trust":
         Sc = 2.0
-        Ce = (np.dot(pre_B_g.reshape(1, len(geom_num_list)*3), pre_move_vector.reshape(len(geom_num_list), 1)) + 0.5 * np.dot(np.dot(pre_move_vector.reshape(1, len(geom_num_list)), model_hess), pre_move_vector.reshape(len(geom_num_list), 1)))
+        Ce = (np.dot(pre_B_g.reshape(1, len(geom_num_list)), pre_move_vector.reshape(len(geom_num_list), 1)) + 0.5 * np.dot(np.dot(pre_move_vector.reshape(1, len(geom_num_list)), model_hess), pre_move_vector.reshape(len(geom_num_list), 1)))
         r = (pre_B_e - B_e) / (Ce)
         print("reference_value_of_trust_radius: ", r)
         r_min = 0.75
