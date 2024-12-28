6fd35a99a8a68530e6932f80c93ec8cf31d62f51
ss0832 <80376408+ss0832@users.noreply.github.com>
2024-12-28 20:18:21 +0900

    Add files via upload
    some improvements

--- relaxed_scan.py
+++ relaxed_scan.py
@@ -1,15 +1,16 @@
 import sys
 import os
 import shutil
-import random
+import datetime
 sys.path.append('./biaspotpy')
 import biaspotpy.calc_tools
 from biaspotpy.parameter import UnitValueLib
 import numpy as np
-
 import biaspotpy
 
-def orientation_search(parser):
+# When you use psi4 with this script, segmentation fault may occur. Thus, I recommend to use pyscf, tblite and so on with the following script.
+
+def relaxed_scan_perser(parser):
     parser.add_argument("-nsample", "--number_of_samples", type=int, default=10, help='the number of sampling relaxed scan coordinates')
     parser.add_argument("-scan", "--scan_tgt", nargs="*", type=str, help='scan target (ex.) [[bond, angle, or dihedral etc.] [atom_num] [(value_1(ex. 1.0 ang.)),(value_2(ex. 1.5 ang.))] ...] ', default=None)
     return parser
@@ -28,10 +29,10 @@ def num_parse(numbers):
     return sub_list
 
 
-def save_xyz_file(coord_list, element_list, file_name, additional_name):
+def save_xyz_file(coord_list, element_list, file_name, additional_name, directory):
     no_ext_file_name = os.path.splitext(file_name)[0]
     sample_file_name = no_ext_file_name+"_RelaxedScan_"+str(additional_name)+".xyz"
-    with open(sample_file_name, 'w') as f:
+    with open(directory + sample_file_name, 'w') as f:
         f.write(str(len(coord_list))+"\n")
         f.write("RelaxedScan_"+str(additional_name)+"\n")
         for i in range(len(coord_list)):
@@ -55,8 +56,12 @@ def make_scan_tgt(scan_list):
 
 if __name__ == '__main__':
     parser = biaspotpy.interface.init_parser()
-    parser = orientation_search(parser)
+    parser = relaxed_scan_perser(parser)
     args = biaspotpy.interface.optimizeparser(parser)
+    date = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+    directory = os.path.splitext(args.INPUT)[0]+"_RScan_"+date+"/"
+    os.makedirs(directory, exist_ok=True)   
+    ene_profile_filepath = directory + os.path.splitext(args.INPUT)[0]+"_scan_energy_profile_"+date+".csv"
     
     scan_part = int(args.number_of_samples)
     scan_tgt_list, atom_num_list, scan_range_list = make_scan_tgt(args.scan_tgt)
@@ -74,13 +79,18 @@ if __name__ == '__main__':
     original_input_file = args.INPUT
     original_args = args
 
-    with open(os.path.splitext(original_input_file)[0]+"_scan_energy_profile.csv", "w") as f:
+    with open(ene_profile_filepath, "w") as f:
         f.write(str(",".join(scan_tgt_list))+",energy,bias_energy\n")
         tmp_atom_num_list = [str(i).replace(",", "_") for i in atom_num_list]
         f.write(str(",".join(tmp_atom_num_list))+",(hartree),(hartree)\n")
 
+
+    shutil.copyfile(original_input_file, directory + os.path.splitext(original_input_file)[0]+"_RelaxedScan_0.xyz")
     shutil.copyfile(original_input_file, os.path.splitext(original_input_file)[0]+"_RelaxedScan_0.xyz")
     args.INPUT = os.path.splitext(original_input_file)[0]+"_RelaxedScan_0.xyz"
+
+    with open(directory + "input.txt", 'w') as f:
+        f.write(str(vars(args)))
 
     for i in range(scan_part):
         print("scan type(s)  : ", scan_tgt_list)
@@ -98,13 +108,14 @@ if __name__ == '__main__':
         energy = bpa.final_energy
         bias_energy = bpa.final_bias_energy
         
-        next_input_file = save_xyz_file(opted_geometry, element_list, original_input_file, i+1)
+        next_input_file_for_save = directory + save_xyz_file(opted_geometry, element_list, original_input_file, i+1, directory)
+        next_input_file = save_xyz_file(opted_geometry, element_list, original_input_file, i+1, directory)
         args = original_args
         args.INPUT = next_input_file
         print("Done.")
         
-        with open(os.path.splitext(original_input_file)[0]+"_scan_energy_profile.csv", "a") as f:
+        with open(ene_profile_filepath, "a") as f:
             f.write(str(",".join(list(map(str, scan_job_list[i].tolist())))) + "," + str(energy) + "," + str(bias_energy) + "\n")
-  
+        del bpa
     
     print("Relaxed scan done ...")
