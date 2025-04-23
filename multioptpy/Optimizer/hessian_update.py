40dc50f57648741dcae92a75f65baa1a8fcd354f
ss0832 <80376408+ss0832@users.noreply.github.com>
2025-04-23 16:00:59 +0900

    Add files via upload

--- multioptpy/Optimizer/hessian_update.py
+++ multioptpy/Optimizer/hessian_update.py
@@ -11,18 +11,19 @@ MSP
 class ModelHessianUpdate:
     def __init__(self):
         self.Initialization = True
+        self.denom_threshold = 1e-10
         return
     def flowchart_hessian_update(self, hess, displacement, delta_grad, method):
         #Theor Chem Acc  (2016) 135:84 
         z = delta_grad - np.dot(hess, delta_grad)
         
         zs_denominator = np.linalg.norm(displacement) * np.linalg.norm(z)
-        if abs(zs_denominator) < 1e-10:
-            zs_denominator += 1e-10
+        if abs(zs_denominator) < self.denom_threshold:
+            zs_denominator += self.denom_threshold
         zs = np.dot(z.T, displacement) / zs_denominator
         ys_denominator = np.linalg.norm(displacement) * np.linalg.norm(delta_grad)
-        if abs(ys_denominator) < 1e-10:
-            ys_denominator += 1e-10
+        if abs(ys_denominator) < self.denom_threshold:
+            ys_denominator += self.denom_threshold
         
         ys = np.dot(delta_grad.T, displacement) / ys_denominator
         if zs < -0.1:
@@ -41,11 +42,11 @@ class ModelHessianUpdate:
     
     def BFGS_hessian_update(self, hess, displacement, delta_grad):
         demon_1 = np.dot(displacement.T, delta_grad)
-        if np.abs(demon_1) < 1e-10:
-            demon_1 += 1e-10
+        if np.abs(demon_1) < self.denom_threshold:
+            return np.zeros((len(delta_grad), len(delta_grad)))
         demon_2 = np.dot(np.dot(displacement.T, hess), displacement)
-        if np.abs(demon_2) < 1e-10:
-            demon_2 += 1e-10
+        if np.abs(demon_2) < self.denom_threshold:
+            return np.zeros((len(delta_grad), len(delta_grad)))
         
         delta_hess = (np.dot(delta_grad, delta_grad.T)) / demon_1 - (np.dot(np.dot(np.dot(hess, displacement) , displacement.T), hess.T)/ demon_2)
         return delta_hess
@@ -54,20 +55,20 @@ class ModelHessianUpdate:
         #J. Chem. Phys. 1999, 111, 10806
         A = delta_grad - np.dot(hess, displacement)
         delta_hess_SR1_denominator = np.dot(A.T, displacement)
-        if np.abs(delta_hess_SR1_denominator) < 1e-10:
-            delta_hess_SR1_denominator += 1e-10
+        if np.abs(delta_hess_SR1_denominator) < self.denom_threshold:
+            return np.zeros((len(delta_grad), len(delta_grad)))
         delta_hess_SR1 = np.dot(A, A.T) / delta_hess_SR1_denominator
         delta_hess_BFGS_denominator_1 = np.dot(displacement.T, delta_grad)
-        if np.abs(delta_hess_BFGS_denominator_1) < 1e-10:
-            delta_hess_BFGS_denominator_1 += 1e-10
+        if np.abs(delta_hess_BFGS_denominator_1) < self.denom_threshold:
+            return np.zeros((len(delta_grad), len(delta_grad)))
         delta_hess_BFGS_denominator_2 = np.dot(np.dot(displacement.T, hess), displacement)
-        if np.abs(delta_hess_BFGS_denominator_2) < 1e-10:
-            delta_hess_BFGS_denominator_2 += 1e-10
+        if np.abs(delta_hess_BFGS_denominator_2) < self.denom_threshold:
+            return np.zeros((len(delta_grad), len(delta_grad)))
         
         delta_hess_BFGS = (np.dot(delta_grad, delta_grad.T) / delta_hess_BFGS_denominator_1) - (np.dot(np.dot(np.dot(hess, displacement) , displacement.T), hess.T)/ delta_hess_BFGS_denominator_2)
         Bofill_const_denominator = np.dot(np.dot(np.dot(A.T, A), displacement.T), displacement)
-        if np.abs(Bofill_const_denominator) < 1e-10:
-            Bofill_const_denominator += 1e-10
+        if np.abs(Bofill_const_denominator) < self.denom_threshold:
+            Bofill_const_denominator += self.denom_threshold
         
         Bofill_const = np.dot(np.dot(np.dot(A.T, displacement), A.T), displacement) / Bofill_const_denominator
         delta_hess = np.sqrt(Bofill_const)*delta_hess_SR1 + (1 - np.sqrt(Bofill_const))*delta_hess_BFGS
@@ -77,15 +78,15 @@ class ModelHessianUpdate:
         #J. Chem. Phys. 1999, 111, 10806
         A = delta_grad - np.dot(hess, displacement)
         delta_hess_SR1_denominator = np.dot(A.T, displacement)
-        if np.abs(delta_hess_SR1_denominator) < 1e-10:
-            delta_hess_SR1_denominator += 1e-10
+        if np.abs(delta_hess_SR1_denominator) < self.denom_threshold:
+            return np.zeros((len(delta_grad), len(delta_grad)))
             
         delta_hess_SR1 = np.dot(A, A.T) / delta_hess_SR1_denominator
          
         block_1 = delta_grad - 1 * np.dot(hess, displacement) 
         block_2_denominator = np.dot(displacement.T, displacement)
-        if np.abs(block_2_denominator) < 1e-10:
-            block_2_denominator += 1e-10
+        if np.abs(block_2_denominator) < self.denom_threshold:
+            return np.zeros((len(delta_grad), len(delta_grad)))
         
         block_2 = np.dot(displacement, displacement.T) / block_2_denominator
         
@@ -99,8 +100,8 @@ class ModelHessianUpdate:
         #J. Chem. Phys. 1999, 111, 10806
         A = delta_grad - np.dot(hess, displacement)
         delta_hess_SR1_denominator = np.dot(A.T, displacement)
-        if np.abs(delta_hess_SR1_denominator) < 1e-10:
-            delta_hess_SR1_denominator += 1e-10
+        if np.abs(delta_hess_SR1_denominator) < self.denom_threshold:
+            return np.zeros((len(delta_grad), len(delta_grad)))
         delta_hess_SR1 = np.dot(A, A.T) / delta_hess_SR1_denominator
         return delta_hess_SR1
     
@@ -108,8 +109,8 @@ class ModelHessianUpdate:
         #Journal of Molecular Structure: THEOCHEM 2002, 591 (1–3), 35–57.
         block_1 = delta_grad - 1 * np.dot(hess, displacement) 
         block_2_denominator = np.dot(displacement.T, displacement)
-        if np.abs(block_2_denominator) < 1e-10:
-            block_2_denominator += 1e-10
+        if np.abs(block_2_denominator) < self.denom_threshold:
+            return np.zeros((len(delta_grad), len(delta_grad)))
         block_2 = np.dot(displacement, displacement.T) / block_2_denominator ** 2
         
         delta_hess_P = -1 * np.dot(block_1.T, displacement) * block_2 + (np.dot(block_1, displacement.T) + np.dot(displacement, block_1.T)) / block_2_denominator  #PSB
@@ -119,22 +120,22 @@ class ModelHessianUpdate:
         #Journal of Molecular Structure: THEOCHEM 2002, 591 (1–3), 35–57.
         A = delta_grad - np.dot(hess, displacement)
         delta_hess_MS_denominator = np.dot(A.T, displacement)
-        if np.abs(delta_hess_MS_denominator) < 1e-10:
-            delta_hess_MS_denominator += 1e-10
+        if np.abs(delta_hess_MS_denominator) < self.denom_threshold:
+            return np.zeros((len(delta_grad), len(delta_grad)))
         
         delta_hess_MS = np.dot(A, A.T) / delta_hess_MS_denominator #SR1
         block_1 = delta_grad - 1 * np.dot(hess, displacement) 
         block_2_denominator = np.dot(displacement.T, displacement)
-        if np.abs(block_2_denominator) < 1e-10:
-            block_2_denominator += 1e-10
+        if np.abs(block_2_denominator) < self.denom_threshold:
+            return np.zeros((len(delta_grad), len(delta_grad)))
         
         block_2 = np.dot(displacement, displacement.T) / block_2_denominator ** 2
         
         delta_hess_P = -1 * np.dot(block_1.T, displacement) * block_2 + (np.dot(block_1, displacement.T) + np.dot(displacement, block_1.T)) / block_2_denominator  #PSB
         A_norm = np.linalg.norm(A)
         displacement_norm = np.linalg.norm(displacement)
-        if displacement_norm * A_norm < 1e-10:
-            displacement_norm += 1e-10
+        if displacement_norm * A_norm < self.denom_threshold:
+            return np.zeros((len(delta_grad), len(delta_grad)))
             
         
         phi = np.sin(np.arccos(np.dot(displacement.T, A) / (A_norm * displacement_norm))) ** 2
