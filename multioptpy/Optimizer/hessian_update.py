36e721fb42e74de7dc39f1cc1dfc1e5c8421cf3e
ss0832 <80376408+ss0832@users.noreply.github.com>
2025-09-25 20:01:09 +0900

    Add files via upload
    - fix bugs
    - change the algorithms of Hessian update method

--- multioptpy/Optimizer/hessian_update.py
+++ multioptpy/Optimizer/hessian_update.py
@@ -1,5 +1,4 @@
 import numpy as np
-import copy
 
 """
 FSB, Bofill
@@ -14,6 +13,7 @@ class ModelHessianUpdate:
         self.denom_threshold = 1e-10
         return
     def flowchart_hessian_update(self, hess, displacement, delta_grad, method):
+        print("Flowchart Hessian Update")
         #Theor Chem Acc  (2016) 135:84 
         z = delta_grad - np.dot(hess, delta_grad)
         
@@ -41,117 +41,143 @@ class ModelHessianUpdate:
     
     
     def BFGS_hessian_update(self, hess, displacement, delta_grad):
+        print("BFGS Hessian Update")
         demon_1 = np.dot(displacement.T, delta_grad)
-        if np.abs(demon_1) < self.denom_threshold:
-            print("denominator is too small!!! Stop performing Hessian update.")
-            return np.zeros((len(delta_grad), len(delta_grad)))
         demon_2 = np.dot(np.dot(displacement.T, hess), displacement)
-        if np.abs(demon_2) < self.denom_threshold:
-            print("denominator is too small!!! Stop performing Hessian update.")
-            return np.zeros((len(delta_grad), len(delta_grad)))
-        
-        delta_hess = (np.dot(delta_grad, delta_grad.T)) / demon_1 - (np.dot(np.dot(np.dot(hess, displacement) , displacement.T), hess.T)/ demon_2)
+        delta_hess = np.zeros((len(delta_grad), len(delta_grad)))
+        if np.abs(demon_1) >= self.denom_threshold:
+            term1 = np.dot(delta_grad, delta_grad.T) / demon_1
+        else:
+            print("demon_1 is too small, term1 set to zero.")
+            term1 = np.zeros((len(delta_grad), len(delta_grad)))
+        if np.abs(demon_2) >= self.denom_threshold:
+            term2 = np.dot(np.dot(np.dot(hess, displacement), displacement.T), hess.T) / demon_2
+        else:
+            print("demon_2 is too small, term2 set to zero.")
+            term2 = np.zeros((len(delta_grad), len(delta_grad)))
+        delta_hess = term1 - term2
         return delta_hess
     
     def FSB_hessian_update(self, hess, displacement, delta_grad):
+        print("FSB Hessian Update")
         #J. Chem. Phys. 1999, 111, 10806
         A = delta_grad - np.dot(hess, displacement)
         delta_hess_SR1_denominator = np.dot(A.T, displacement)
-        if np.abs(delta_hess_SR1_denominator) < self.denom_threshold:
-            print("denominator is too small!!! Stop performing Hessian update.")
-            return np.zeros((len(delta_grad), len(delta_grad)))
-        delta_hess_SR1 = np.dot(A, A.T) / delta_hess_SR1_denominator
+        if np.abs(delta_hess_SR1_denominator) >= self.denom_threshold:
+            delta_hess_SR1 = np.dot(A, A.T) / delta_hess_SR1_denominator
+        else:
+            print("SR1 denominator is too small, term set to zero.")
+            delta_hess_SR1 = np.zeros((len(delta_grad), len(delta_grad)))
         delta_hess_BFGS_denominator_1 = np.dot(displacement.T, delta_grad)
-        if np.abs(delta_hess_BFGS_denominator_1) < self.denom_threshold:
-            print("denominator is too small!!! Stop performing Hessian update.")
-            return np.zeros((len(delta_grad), len(delta_grad)))
+        if np.abs(delta_hess_BFGS_denominator_1) >= self.denom_threshold:
+            term1 = np.dot(delta_grad, delta_grad.T) / delta_hess_BFGS_denominator_1
+        else:
+            print("BFGS denominator 1 is too small, term set to zero.")
+            term1 = np.zeros((len(delta_grad), len(delta_grad)))
         delta_hess_BFGS_denominator_2 = np.dot(np.dot(displacement.T, hess), displacement)
-        if np.abs(delta_hess_BFGS_denominator_2) < self.denom_threshold:
-            print("denominator is too small!!! Stop performing Hessian update.")
-            return np.zeros((len(delta_grad), len(delta_grad)))
-        
-        delta_hess_BFGS = (np.dot(delta_grad, delta_grad.T) / delta_hess_BFGS_denominator_1) - (np.dot(np.dot(np.dot(hess, displacement) , displacement.T), hess.T)/ delta_hess_BFGS_denominator_2)
+        if np.abs(delta_hess_BFGS_denominator_2) >= self.denom_threshold:
+            term2 = np.dot(np.dot(np.dot(hess, displacement), displacement.T), hess.T) / delta_hess_BFGS_denominator_2
+        else:
+            print("BFGS denominator 2 is too small, term set to zero.")
+            term2 = np.zeros((len(delta_grad), len(delta_grad)))
+        delta_hess_BFGS = term1 - term2
+        Bofill_const_numerator = np.dot(np.dot(np.dot(A.T, displacement), A.T), displacement)
         Bofill_const_denominator = np.dot(np.dot(np.dot(A.T, A), displacement.T), displacement)
-        if np.abs(Bofill_const_denominator) < self.denom_threshold:
-            print("denominator is too small!!! Stop performing Hessian update.")
-            return np.zeros((len(delta_grad), len(delta_grad)))
+        Bofill_const = Bofill_const_numerator / Bofill_const_denominator
         
-        Bofill_const = np.dot(np.dot(np.dot(A.T, displacement), A.T), displacement) / Bofill_const_denominator
+        if np.abs(Bofill_const_denominator) >= self.denom_threshold:
+            Bofill_const = Bofill_const_numerator / Bofill_const_denominator
+        else: # For minimization, BFGS is chosen to guarantee positive definiteness
+            Bofill_const = 0.0
+            print("Bofill_const denominator is too small, set to zero.")
+            
+        print("Bofill_const:", Bofill_const)
         delta_hess = np.sqrt(Bofill_const)*delta_hess_SR1 + (1 - np.sqrt(Bofill_const))*delta_hess_BFGS
         return delta_hess
 
     def Bofill_hessian_update(self, hess, displacement, delta_grad):
+        print("Bofill Hessian Update")
         #J. Chem. Phys. 1999, 111, 10806
         A = delta_grad - np.dot(hess, displacement)
         delta_hess_SR1_denominator = np.dot(A.T, displacement)
-        if np.abs(delta_hess_SR1_denominator) < self.denom_threshold:
-            print("denominator is too small!!! Stop performing Hessian update.")
-            return np.zeros((len(delta_grad), len(delta_grad)))
-            
-        delta_hess_SR1 = np.dot(A, A.T) / delta_hess_SR1_denominator
-         
-        block_1 = delta_grad - 1 * np.dot(hess, displacement) 
+        if np.abs(delta_hess_SR1_denominator) >= self.denom_threshold:
+            delta_hess_SR1 = np.dot(A, A.T) / delta_hess_SR1_denominator
+        else:
+            print("SR1 denominator is too small, term set to zero.")
+            delta_hess_SR1 = np.zeros((len(delta_grad), len(delta_grad)))
+        block_1 = delta_grad - 1 * np.dot(hess, displacement)
         block_2_denominator = np.dot(displacement.T, displacement)
-        if np.abs(block_2_denominator) < self.denom_threshold:
-            print("denominator is too small!!! Stop performing Hessian update.")
-            return np.zeros((len(delta_grad), len(delta_grad)))
-        
-        block_2 = np.dot(displacement, displacement.T) / block_2_denominator
-        
-        delta_hess_PSB = -1 * np.dot(block_1.T, displacement) * block_2 + (np.dot(block_1, displacement.T) + np.dot(displacement, block_1.T)) / np.dot(displacement.T, displacement) 
-            
-        Bofill_const = np.dot(np.dot(np.dot(A.T, displacement), A.T), displacement) / np.dot(np.dot(np.dot(A.T, A), displacement.T), displacement)
+        if np.abs(block_2_denominator) >= self.denom_threshold:
+            block_2 = np.dot(displacement, displacement.T) / block_2_denominator
+        else:
+            print("block_2 denominator is too small, term set to zero.")
+            block_2 = np.zeros((len(delta_grad), len(delta_grad)))
+        if np.abs(block_2_denominator) >= self.denom_threshold:
+            delta_hess_PSB = -1 * np.dot(block_1.T, displacement) * block_2 + (np.dot(block_1, displacement.T) + np.dot(displacement, block_1.T)) / block_2_denominator
+        else:
+            print("PSB denominator is too small, term set to zero.")
+            delta_hess_PSB = np.zeros((len(delta_grad), len(delta_grad)))
+        bofill_const_denominator = np.dot(np.dot(np.dot(A.T, A), displacement.T), displacement)
+        bofill_const_numerator = np.dot(np.dot(np.dot(A.T, displacement), A.T), displacement)
+        Bofill_const = bofill_const_numerator / bofill_const_denominator
+        if np.abs(bofill_const_denominator) >= self.denom_threshold:
+            pass
+        else: 
+            Bofill_const = 0.0
+            print("Bofill_const denominator is too small, set to zero.")
+        print("Bofill_const:", Bofill_const)
         delta_hess = Bofill_const*delta_hess_SR1 + (1 - Bofill_const)*delta_hess_PSB
-        return delta_hess  
+        return delta_hess
     
     def SR1_hessian_update(self, hess, displacement, delta_grad):
+        print("SR1 Hessian Update")
         #J. Chem. Phys. 1999, 111, 10806
         A = delta_grad - np.dot(hess, displacement)
         delta_hess_SR1_denominator = np.dot(A.T, displacement)
-        if np.abs(delta_hess_SR1_denominator) < self.denom_threshold:
-            print("denominator is too small!!! Stop performing Hessian update.")
-            return np.zeros((len(delta_grad), len(delta_grad)))
-        delta_hess_SR1 = np.dot(A, A.T) / delta_hess_SR1_denominator
+        if np.abs(delta_hess_SR1_denominator) >= self.denom_threshold:
+            delta_hess_SR1 = np.dot(A, A.T) / delta_hess_SR1_denominator
+        else:
+            print("SR1 denominator is too small, term set to zero.")
+            delta_hess_SR1 = np.zeros((len(delta_grad), len(delta_grad)))
         return delta_hess_SR1
     
     def PSB_hessian_update(self, hess, displacement, delta_grad):
+        print("PSB Hessian Update")
         #Journal of Molecular Structure: THEOCHEM 2002, 591 (1–3), 35–57.
-        block_1 = delta_grad - 1 * np.dot(hess, displacement) 
+        block_1 = delta_grad - 1 * np.dot(hess, displacement)
         block_2_denominator = np.dot(displacement.T, displacement)
-        if np.abs(block_2_denominator) < self.denom_threshold:
-            print("denominator is too small!!! Stop performing Hessian update.")
-            return np.zeros((len(delta_grad), len(delta_grad)))
-        block_2 = np.dot(displacement, displacement.T) / block_2_denominator ** 2
-        
-        delta_hess_P = -1 * np.dot(block_1.T, displacement) * block_2 + (np.dot(block_1, displacement.T) + np.dot(displacement, block_1.T)) / block_2_denominator  #PSB
+        if np.abs(block_2_denominator) >= self.denom_threshold:
+            block_2 = np.dot(displacement, displacement.T) / block_2_denominator ** 2
+            delta_hess_P = -1 * np.dot(block_1.T, displacement) * block_2 + (np.dot(block_1, displacement.T) + np.dot(displacement, block_1.T)) / block_2_denominator
+        else:
+            print("PSB denominator is too small, term set to zero.")
+            delta_hess_P = np.zeros((len(delta_grad), len(delta_grad)))
         return delta_hess_P
     
     def MSP_hessian_update(self, hess, displacement, delta_grad):
+        print("MSP Hessian Update")
         #Journal of Molecular Structure: THEOCHEM 2002, 591 (1–3), 35–57.
         A = delta_grad - np.dot(hess, displacement)
         delta_hess_MS_denominator = np.dot(A.T, displacement)
-        if np.abs(delta_hess_MS_denominator) < self.denom_threshold:
-            print("denominator is too small!!! Stop performing Hessian update.")
-            return np.zeros((len(delta_grad), len(delta_grad)))
-        
-        delta_hess_MS = np.dot(A, A.T) / delta_hess_MS_denominator #SR1
-        block_1 = delta_grad - 1 * np.dot(hess, displacement) 
+        if np.abs(delta_hess_MS_denominator) >= self.denom_threshold:
+            delta_hess_MS = np.dot(A, A.T) / delta_hess_MS_denominator #SR1
+        else:
+            print("MS denominator is too small, term set to zero.")
+            delta_hess_MS = np.zeros((len(delta_grad), len(delta_grad)))
+        block_1 = delta_grad - 1 * np.dot(hess, displacement)
         block_2_denominator = np.dot(displacement.T, displacement)
-        if np.abs(block_2_denominator) < self.denom_threshold:
-            print("denominator is too small!!! Stop performing Hessian update.")
-            return np.zeros((len(delta_grad), len(delta_grad)))
-        
-        block_2 = np.dot(displacement, displacement.T) / block_2_denominator ** 2
-        
-        delta_hess_P = -1 * np.dot(block_1.T, displacement) * block_2 + (np.dot(block_1, displacement.T) + np.dot(displacement, block_1.T)) / block_2_denominator  #PSB
+        if np.abs(block_2_denominator) >= self.denom_threshold:
+            block_2 = np.dot(displacement, displacement.T) / block_2_denominator ** 2
+            delta_hess_P = -1 * np.dot(block_1.T, displacement) * block_2 + (np.dot(block_1, displacement.T) + np.dot(displacement, block_1.T)) / block_2_denominator
+        else:
+            print("PSB denominator is too small, term set to zero.")
+            delta_hess_P = np.zeros((len(delta_grad), len(delta_grad)))
         A_norm = np.linalg.norm(A)
         displacement_norm = np.linalg.norm(displacement)
-        if displacement_norm * A_norm < self.denom_threshold:
-            print("denominator is too small!!! Stop performing Hessian update.")
-            return np.zeros((len(delta_grad), len(delta_grad)))
-            
-        
-        phi = np.sin(np.arccos(np.dot(displacement.T, A) / (A_norm * displacement_norm))) ** 2
+        if displacement_norm * A_norm >= self.denom_threshold:
+            phi = np.sin(np.arccos(np.dot(displacement.T, A) / (A_norm * displacement_norm))) ** 2
+        else:
+            print("phi denominator is too small, set phi=0.")
+            phi = 0.0
         delta_hess = phi*delta_hess_P + (1 - phi)*delta_hess_MS
-        
         return delta_hess
