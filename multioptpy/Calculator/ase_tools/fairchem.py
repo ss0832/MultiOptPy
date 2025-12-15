c2a6a8ba9be74a27b736d9aac1c47c770dbce5bc
ss0832 <80376408+ss0832@users.noreply.github.com>
2025-12-15 13:52:42 +0900

    Add files via upload

--- multioptpy/Calculator/ase_tools/fairchem.py
+++ multioptpy/Calculator/ase_tools/fairchem.py
@@ -9,20 +9,23 @@ class ASE_FAIRCHEM:
         self.device_mode = kwargs.get('device_mode', "cpu")
         self.software_type = kwargs.get('software_type', None)
         print(f"ASE_FAIRCHEM: software_type = {self.software_type}")
+        try:
+            from fairchem.core import FAIRChemCalculator
+            self.FAIRChemCalculator = FAIRChemCalculator
+            from fairchem.core.units.mlip_unit import load_predict_unit
+            self.load_predict_unit = load_predict_unit
+        except ImportError:
+            raise ImportError("FAIRChem.core modules not found")
         
     
     def run(self): # fairchem.core: version 2.x.x
-        try:
-            from fairchem.core import FAIRChemCalculator
-            from fairchem.core.units.mlip_unit import load_predict_unit
-        except ImportError:
-            raise ImportError("FAIRChem.core modules not found")
+        
         # Load the prediction unit
-        predict_unit = load_predict_unit(path=self.software_path, device=self.device_mode)
+        predict_unit = self.load_predict_unit(path=self.software_path, device=self.device_mode)
         print(f"ASE_FAIRCHEM: device_mode = {self.device_mode}")
         print(f"ASE_FAIRCHEM: task_name = {self.task_name}")
         # Set up the FAIRChem calculator
-        fairchem_calc = FAIRChemCalculator(predict_unit=predict_unit, task_name=self.task_name)
+        fairchem_calc = self.FAIRChemCalculator(predict_unit=predict_unit, task_name=self.task_name)
         self.atom_obj.info = {"charge": int(self.electric_charge_and_multiplicity[0]),
                               "spin": int(self.electric_charge_and_multiplicity[1])}
         self.atom_obj.calc = fairchem_calc
