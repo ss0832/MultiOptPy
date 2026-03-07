03138d39f37cb3ce121fbfdc49a6bb1222460dcf
ss0832 <80376408+ss0832@users.noreply.github.com>
2026-03-07 11:14:31 +0900

    Delete nebmain.py

--- nebmain.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append('./multioptpy')
-
-import multioptpy
-
-parser = multioptpy.interface.init_parser()
-args = multioptpy.interface.nebparser(parser)
-NEB = multioptpy.neb.NEB(args)
-NEB.run()
