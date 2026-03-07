526ddcad5b162993c8ab217811bf9d0be15d90d8
ss0832 <80376408+ss0832@users.noreply.github.com>
2026-03-07 11:25:22 +0900

    Implement versioning for MultiOptPy
    Add version retrieval for MultiOptPy package.

--- multioptpy/__init__.py
+++ multioptpy/__init__.py
@@ -1,5 +1,13 @@
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("MultiOptPy")
+except PackageNotFoundError:
+    __version__ = "unknown"
+  
 from . import interface
 from . import neb
 from . import ieip
 from . import moleculardynamics
 from . import optimization
+
