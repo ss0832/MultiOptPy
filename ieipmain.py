86b2af36e8dc8660e96dde30df1ff043014c87cf
ss0832 <80376408+ss0832@users.noreply.github.com>
2026-03-07 11:14:13 +0900

    Delete ieipmain.py

--- ieipmain.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append('./multioptpy')
-
-import multioptpy
-
-parser = multioptpy.interface.init_parser()
-args = multioptpy.interface.ieipparser(parser)
-iEIP = multioptpy.ieip.iEIP(args)
-iEIP.run()
