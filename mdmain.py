bcdb5311800a94bc18ca4123925302b9d50a9ab7
ss0832 <80376408+ss0832@users.noreply.github.com>
2026-03-07 11:14:22 +0900

    Delete mdmain.py

--- mdmain.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append('./multioptpy')
-
-import multioptpy
-
-parser = multioptpy.interface.init_parser()
-args = multioptpy.interface.mdparser(parser)
-MD = multioptpy.moleculardynamics.MD(args)
-MD.run()
